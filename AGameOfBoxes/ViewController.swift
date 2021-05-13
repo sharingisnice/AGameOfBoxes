85006896d4cb3cc022b5a08dea4e128703667647
Mert <mert.ejd@gmail.com>
2021-05-13 22:26:38 +0300

    viewModel made optional

--- AGameOfBoxes/ViewController.swift
+++ AGameOfBoxes/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var restartButtonOutlet: UIButton!
     
     
-    var viewModel = GameScreenViewModel()
+    var viewModel : GameScreenViewModel? = GameScreenViewModel()
     var setupComplete = false
     
     override func viewDidLoad() {
@@ -32,29 +32,36 @@ class ViewController: UIViewController {
             setupUI()
         }
     }
-
+    
     
     func setupUI() {
-        let initialView = viewModel.DrawBlocks(size: 5, enclosingView: gameArea)
-        
-        for sub in initialView {
-            gameArea.addSubview(sub)
+        if let viewModel = viewModel {
+            
+            let initialView = viewModel.DrawBlocks(size: 5, enclosingView: gameArea)
+            
+            for sub in initialView {
+                gameArea.addSubview(sub)
+            }
+            
+            viewModel.delegate = self
+            setupComplete = true
+            
+            restartButtonOutlet.isEnabled = false
+            
         }
-        
-        viewModel.delegate = self
-        setupComplete = true
-        
-        restartButtonOutlet.isEnabled = false
     }
     
     func updateUI() {
-        remainingManoeuvresLabel.text = "Remaining Moves: \(viewModel.remainingManoeuvres)"
-        totalScoreLabel.text = "Total Score: \(viewModel.totalScore)"
+        if let viewModel = viewModel {
+            remainingManoeuvresLabel.text = "Remaining Moves: \(viewModel.remainingManoeuvres)"
+            totalScoreLabel.text = "Total Score: \(viewModel.totalScore)"
+        }
     }
     
     @IBAction func restartGame(_ sender: Any) {
+        viewModel = nil
         viewModel = GameScreenViewModel()
-
+        setupComplete = false
         setupUI()
         updateUI()
         
